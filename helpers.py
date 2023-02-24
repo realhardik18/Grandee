2649157cced4eea840c1c74a2b13bf098f99dcc4
realhardik18 <realhardik18@gmail.com>
2023-02-24 23:54:13 +0530

    got twitter data retrieval done

--- helpers.py
+++ helpers.py
@@ -9,5 +9,31 @@ class Twitter:
     def GetUsernameFromLink(user_link):
         #https://twitter.com/realhardik18
         return user_link[user_link.index('.com/')+5:]
+    def GetUserID(user_link):
+        return client.get_user(username=Twitter.GetUsernameFromLink(user_link=user_link)).data['id']
+    def GetUserFollowers(user_link):
+        return client.get_user(username=Twitter.GetUsernameFromLink(user_link=user_link),user_fields=['public_metrics']).data['public_metrics']['followers_count']
+    def GetUserFollowing(user_link):
+        return client.get_user(username=Twitter.GetUsernameFromLink(user_link=user_link),user_fields=['public_metrics']).data['public_metrics']['following_count']
+    def GetUserTweetCount(user_link):
+        return client.get_user(username=Twitter.GetUsernameFromLink(user_link=user_link),user_fields=['public_metrics']).data['public_metrics']['tweet_count']
+    def GetRecentTweet(user_link):
+        tweet = client.get_users_tweets(Twitter.GetUserID(user_link=user_link)).data[0]
+        metrics=client.get_tweet(tweet.id,tweet_fields=['public_metrics']).data['public_metrics']
+        data={
+            'id':tweet.id,
+            'content':tweet.text,
+            "likes":metrics['like_count'],
+            "views":metrics['impression_count']
+        }
+        return data
+        
+    
 
-print(Twitter.GetUsernameFromLink('https://twitter.com/Ultimateadi18'))
+
+#print(Twitter.GetUsernameFromLink('https://twitter.com/Ultimateadi18'))
+#print(Twitter.GetUserFollowers(user_link='https://twitter.com/Ultimateadi18'))
+#print(Twitter.GetUserFollowing(user_link='https://twitter.com/Ultimateadi18'))
+#print(Twitter.GetUserID(user_link='https://twitter.com/Ultimateadi18'))
+#print(Twitter.GetUserweetCount('https://twitter.com/Ultimateadi18'))
+#print(Twitter.GetRecentTweet('https://twitter.com/Ultimateadi18'))
