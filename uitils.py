09d5ff66ba24b151ea0a9c176812fc423df7d8dc
hardik singh <realhardik18@gmail.com>
2025-01-31 10:43:52 +0530

    refining prompt

--- uitils.py
+++ uitils.py
@@ -19,5 +19,38 @@ def generateimage(prompt,path):
 
     print(f"Image saved in {path}")    
 
-#def generate_system_prompt():
-    #client = Together(api_key=os.getenv('API_KEY'))    
+def generate_script(prompt,path):
+    client = Together(api_key=os.getenv('API_KEY'))    
+    response = client.chat.completions.create(
+        model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo-128K",
+        messages=[
+            {"role": "system", "content": "You are an AI that generates short scripts for voiceovers. Respond strictly with only the script, with no introductions, explanations, or extra text. The script should be engaging, natural-sounding, and within 35-45 seconds of speaking time.Make sure the script is just text and nothing else"},
+            {"role": "user", "content": prompt}
+        ],
+    )
+
+    script=response.choices[0].message.content
+    with open(path, "w") as file:
+        file.write(script) 
+
+def data_generator(path_to_script):    
+    with open("prompts/system_prompt.txt", "r", encoding="utf-8") as file:
+        system_prompt=file.read()
+
+    with open(path_to_script, "r", encoding="utf-8") as file:
+        user_prompt=file.read()        
+
+    client = Together(api_key=os.getenv('API_KEY'))    
+    response = client.chat.completions.create(
+        model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo-128K",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+    )
+
+    script=response.choices[0].message.content
+    with open("prompts/data.txt", "w") as file:
+        file.write(script) 
+
+data_generator("prompts/script.txt")
